d35c39f295f6c975c77a96327d485561ac9260e5
DinhLeHoan <dinhlehoan21101@gmail.com>
2024-02-27 23:26:09 +0700

    comple bill

--- src/main/java/pc05401/assignment/repository/VoucherDetailRepository.java
+++ src/main/java/pc05401/assignment/repository/VoucherDetailRepository.java
@@ -10,9 +10,14 @@ import pc05401.assignment.entity.Voucher;
 import pc05401.assignment.entity.VoucherDetail;
 
 public interface VoucherDetailRepository extends JpaRepository<VoucherDetail, Integer> {
+
+    @Query("DELETE FROM VoucherDetail vd WHERE vd.bill.billId = :billId")
+    @Modifying
+    void deleteByBill_BillId(@Param("billId") int billId);
+
     @Query("SELECT vd FROM VoucherDetail vd WHERE vd.bill.billId = :billId")
     VoucherDetail findByBillId(@Param("billId") int billId);
-    
+
     @Modifying
     @Query("UPDATE VoucherDetail vd SET vd.voucher = :voucher WHERE vd.bill.billId = :billId")
     void updateVoucherDetail(@Param("billId") int billId, @Param("voucher") Voucher voucher);
